cd012d0942cbbf49008ce70c42d2bf5017ab04b9
Borschik <47531847+Borschik1@users.noreply.github.com>
2021-12-10 12:30:25 +0500

    Add files via upload

---
+++ Dijkstra.js
@@ -0,0 +1,51 @@
+function priority(symb) {
+	if (symb == '+' || symb == '-') {
+		return 0;
+	} else if (symb == '*' || symb == '/') {
+		return 1;
+	} else if (symb == '^') {
+		return 2;
+	} else {
+		return -1;
+	}
+}
+
+let fs = require('fs');
+let arg = process.argv;
+
+inputData = fs.readFileSync('input1.txt');
+inputData = inputData.toString();
+
+stack = new Array();
+let ip = 0;
+let outputData = '';
+
+for (let i = 0; i < inputData.length; ++i) {
+	if (isNaN(inputData.charAt(i))) {
+		if (inputData.charAt(i) === '(') {
+			stack[ip] = inputData.charAt(i);
+			ip++;
+			continue;
+		} if (inputData.charAt(i) === ')') {
+			while (stack[ip - 1] != '(') {
+				outputData += stack[ip - 1];
+				ip--;
+			}
+			ip--;
+			continue;
+		}
+		if (priority(inputData.charAt(i)) <= priority(stack[ip - 1])) {
+			outputData += stack[ip - 1];
+			stack[ip - 1] = inputData.charAt(i);
+		} else {
+			stack[ip] = inputData.charAt(i);
+			ip++;
+		}
+	} else {
+		outputData += inputData.charAt(i);
+	}
+}
+for (let i = 0; i < ip; ++i) {
+	outputData += stack[ip - i - 1];
+}
+console.log(outputData);
