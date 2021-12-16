756abca3aa9ef8495372278d3383a195f2c9c642
Borschik <47531847+Borschik1@users.noreply.github.com>
2021-12-16 20:24:27 +0500

    Update Dijkstra.js

--- Dijkstra.js
+++ Dijkstra.js
@@ -10,22 +10,35 @@ function priority(symb) {
 	}
 }
 
+
 function main(inputData) {
 	stack = new Array();
-	let ip = 0;
-	let outputData = '';
+	let ip = 0, now = 0;
+	let number = '';
+	outputData = new Array();
 
 	for (let i = 0; i < inputData.length; ++i) {
 		if (isNaN(inputData.charAt(i))) {
+			if (inputData.charAt(i) == '.') {
+				number += '.';
+				continue;
+			}
+			if (number != '') {
+				outputData[now] = number * 1;
+				now++;
+				number = '';
+			}
 			if (inputData.charAt(i) == '(') {
 				stack[ip] = inputData.charAt(i);
 				ip++;
 				continue;
-			} if (inputData.charAt(i) == ')') {
+			}
+			if (inputData.charAt(i) == ')') {
 				while (stack[ip - 1] != '(') {
-					outputData += stack[ip - 1];
+					outputData[now] = stack[ip - 1];
+					now++;
 					ip--;
-					if (ip == 0) {
+					if (ip < 0) {
 						console.log('Error. Missing open parenthesis.');
 						return 0;
 					}
@@ -34,20 +47,58 @@ function main(inputData) {
 				continue;
 			}
 			if (priority(inputData.charAt(i)) <= priority(stack[ip - 1])) {
-				outputData += stack[ip - 1];
+				outputData[now] = stack[ip - 1];
+				now++;
 				stack[ip - 1] = inputData.charAt(i);
 			} else {
 				stack[ip] = inputData.charAt(i);
 				ip++;
 			}
 		} else {
-			outputData += inputData.charAt(i);
+			number += inputData.charAt(i);
 		}
 	}
-	for (let i = 0; i < ip; ++i) {
-		outputData += stack[ip - i - 1];
+	if (number != '') {
+		outputData[now] = number * 1;
+		now++;
+		number = '';
 	}
-	console.log(outputData);
+	for (let i = 0; i < ip; ++i) {
+		outputData[now] = stack[ip - i - 1];
+		now++;
+	}
+	console.log(outputData.join(' '));
+	
+	
+	ip = 0;
+	for (let i = 0; i < now; i++) {
+		switch(outputData[i]) {
+			case '+':
+				stack[ip - 2] += stack[ip - 1];
+				ip--;
+				break;
+			case '-':
+				stack[ip - 2] -= stack[ip - 1];
+				ip--;
+				break;
+			case '*':
+				stack[ip - 2] *= stack[ip - 1];
+				ip--;;
+				break;
+			case '/':
+				stack[ip - 2] /= stack[ip - 1];
+				ip--;
+				break;
+			case '^':
+				stack[ip - 2] = Math.pow(stack[ip - 2], stack[ip - 1]);
+				ip--;
+				break;
+			default:
+				stack[ip] = outputData[i];
+				ip++;
+		}
+	}
+	console.log(stack[0]);
 }
 
 let fs = require('fs');
